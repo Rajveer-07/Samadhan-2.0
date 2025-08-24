0c0a8cbf9d53c1d7cd161879b2c1d4573732ea1d
Rajveer Dangi <dangirajveer33@gmail.com>
2025-08-24 16:05:33 +0530

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,19 @@
+// Day 1 Mini Task: Print student details from an object
+// code.calmchase.com hackathon - Rajveer
+
+const student = {
+  name: "Rajveer Singh",
+  age: 19,
+  college: "SOIT",
+  roll_no: "CS101" };
+
+// ab iski details ko print karna hai terminal me
+// console.log() use karke.. dekhte hai chalta hai ya nahi
+
+console.log("--- Student Details ---");
+console.log("Name:", student.name); // naam print ho gaya.. nice!
+console.log("Age:", student.age);
+console.log("College:", student.college);
+console.log("Roll No:", student.roll_no); // pehle error aaya tha kyunki maine comma (,) nahi lagaya tha object me
+
+// Done! lagta hai ye sahi hai.
